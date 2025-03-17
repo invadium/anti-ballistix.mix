d6489db9bc1cdb0ddfced490f9ce7827d2ea202f
Igor Khotin <khotin@gmx.com>
2025-03-17 03:38:45 +0200

    [$] .ml file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# anti-ballistic.mix
-Z-Alert: Anti-Ballistic
+# Z-Alert: Anti-Ballistix
+
+## Concept
+
+**The Theme: Aerial attack on a modern city**
+
+**The Goal: Protect the infrastructure at all costs!**
+
+* Control anti-aircraft crews and knock off incoming projectiles
+* A spiritual successor of Missile Command
+* Local co-op
+* Relax with the family during Air Raid Alerts
+* The city lights in the background show the state of the infrastructure - keep the lights glowing!
+* Meta: buy ammunition, upgrade air defences
+
---
+++ anti-ballistix.ml
@@ -0,0 +1,45 @@
+# core
+> state control
+> title screen
+> menu subsystem
+> options submenu
+> tuning console
+> flak guns
+> keyboard/gamepad control subsystem
+> flak autocapture by players
+> AA projectiles
+> drones
+> collision detection
+> vectorized explosions
+> city in the background
+> infrastructure layer
+> drone attacks with explosions
+> infrastructure damage
+> core sfx
+> game stat subsystem
+> score overlay
+> mission success/fail conditions
+> end of the mission screen (with stat and infographics)
+
+# advanced
+>>> more projectiles
+    > ballistic missiles
+    > cruise missiles
+    > aeroballistic missiles
+    > hypersonic missiles
+    > glide bombs
+    > various drones
+>>> more AA systems
+    > missile interceptors
+    > advanced radars?
+    > hints on the incoming next and direction (based on advanced radar data?)
+> local high score table
+
+# meta
+> actual air raid alert checker and indicator
+> SimCity-like newspaper with recent news and attack stats & infographics (launched/intercepted)
+> Tycoon elements - buy ammunition, upgrade defences etc...
+
+# future
+> global high score table?
+
