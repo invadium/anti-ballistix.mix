ba51069e54b9628f3bb2739e293632998574666d
Igor Khotin <khotin@gmx.com>
2025-05-18 01:27:50 +0300

    [*] ground gradient

--- mod/lab/backdrop/sky.js
+++ mod/lab/backdrop/sky.js
@@ -11,19 +11,27 @@ function draw() {
     const w = ctx.width,
           h = env.tune.horizonLine * ctx.height
 
-    const gradient = ctx.createLinearGradient(0, 0, 0, h)
+    // === ground ===
+    //fill('#2c0148')
+    const groundGradient = ctx.createLinearGradient(0, h, 0, ctx.height)
+    groundGradient.addColorStop( 0, '#3c1158')
+    groundGradient.addColorStop(.7, '#1c0138')
+    groundGradient.addColorStop( 1, '#0c0108')
 
-    gradient.addColorStop( 0, '#0c0979')
-    gradient.addColorStop(.5, '#6f0cc3') // purple middle
-    gradient.addColorStop( 1, 'purple')
+    fill(groundGradient)
+    rect(0, h, ctx.width, ctx.height - h)
+
+    // === sky ===
+    const skyGradient = ctx.createLinearGradient(0, 0, 0, h)
+
+    skyGradient.addColorStop( 0, '#0c0979')
+    skyGradient.addColorStop(.5, '#6f0cc3') // purple middle
+    skyGradient.addColorStop( 1, 'purple')
 
     //fill('#3c0197')
-    fill(gradient)
+    fill(skyGradient)
     rect(0, 0, w, h)
 
-    // ground
-    fill('#2c0148')
-    rect(0, h, ctx.width, ctx.height - h)
     //background('#6f0cc3')
     //fill('#ff0000')
     //rect(0, 0, .5 * ctx.width, .5 * ctx.height)
