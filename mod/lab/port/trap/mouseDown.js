0ee9b89d36baf8505d929442730256fc5b7ef0a0
Igor Khotin <khotin@gmx.com>
2026-05-03 02:33:40 +0300

    [#] dump coordinates on ctrl+click

--- mod/lab/port/trap/mouseDown.js
+++ mod/lab/port/trap/mouseDown.js
@@ -1,9 +1,21 @@
 function mouseDown(e) {
     if (env.debug && e.ctrlKey) {
+        // dump coordinates
+        const cam = lab.port,
+              wx  = floor(cam.lx(e.x)),
+              wy  = floor(cam.ly(e.y)),
+              pW  = floor(cam.width()),
+              pH  = floor(cam.height())
+        log(`screen/viewport: ${e.x}:${e.y} -- [${lab.w}:${lab.h}]`)
+        log(`world: ${wx}:${wy} -- [${pW}:${pH}]`)
+        log(`sky: ${at.sky.lx(wx) | 0}:${at.sky.ly(wy) | 0} -- [${at.sky.width() | 0}:${at.sky.height() | 0}]`)
+        log(`ground: ${at.g.lx(wx) | 0}:${at.g.ly(wy) | 0} -- [${at.g.width() | 0}:${at.g.height() | 0}]`)
+        log(`battleground: ${at.bg.lx(wx) | 0}:${at.bg.ly(wy) | 0} -- [${at.bg.width() | 0}:${at.bg.height() | 0}]`)
+
+        // pick entities
         const ls = []
         this.__.pick(e.x, e.y, ls)
 
-        dir(ls)
         if (ls.length === 0) {
             log.raw('=== none ===')
         } else if (ls.length === 1) {
